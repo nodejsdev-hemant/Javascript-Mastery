f5b3f865120f62e80825e328979bedcecc2731d5
Hemant-dev <nodejsdev.hemant@gmail.com>
2022-06-07 17:50:55 +0530

    changes

--- arrayProblems.js
+++ arrayProblems.js
@@ -3,9 +3,16 @@ const customReverse = (array) => {
   const newArray = [...array];
   let lastIndex = array.length - 1;
   for (let firstIndex = 0; firstIndex < lastIndex; firstIndex++) {
-    let temp = newArray[firstIndex];
-    newArray[firstIndex] = newArray[lastIndex];
-    newArray[lastIndex] = temp;
+    //   swapping values using es6 syntax ---- [a,b] = [b,a]
+    [newArray[firstIndex], newArray[lastIndex]] = [
+      newArray[lastIndex],
+      newArray[firstIndex],
+    ];
+    //   swapping values using temp variable ----
+
+    // let temp = newArray[firstIndex];
+    // newArray[firstIndex] = newArray[lastIndex];
+    // newArray[lastIndex] = temp;
     lastIndex--;
   }
   return newArray;
